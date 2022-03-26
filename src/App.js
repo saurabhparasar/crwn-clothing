38f80f67bb6e3ac460dbd73b0fcfb57f18262416
saurabhparasar <saurabhparasar3010@gmail.com>
2022-03-26 18:46:30 +0530

    updating the project with shop component , added the collection-preview component and a collection-item component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} exact />
+        <Route path="/shop" element={<ShopPage />} exact />
+      </Routes>
     </div>
   );
 }
